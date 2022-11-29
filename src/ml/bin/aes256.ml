78a5798da1bc0cf444d1d43a3ba6c9a4a042a3b4
Alexander Diemand <codieplusplus@apax.net>
2022-11-29 22:08:57 +0000

    ml_add_functions (#12)
    ML: +aes256, +sha256
    Signed-off-by: Alexander Diemand <codieplusplus@apax.net>

--- src/ml/bin/aes256.ml
+++ src/ml/bin/aes256.ml
@@ -1,5 +1,6 @@
 open Elykseer_crypto
 open Mlcpp_cstdio
+open Mlcpp_filesystem
 
 let k = Key256.from_hex "1234567890123456789012345678901234567890123456789012345678901234"
 
@@ -8,11 +9,10 @@ let iv = Key128.from_hex "12345678901234567890123456789012"
 let read_file fn =
   Cstdio.File.fopen fn "r" |> function
   | Ok fptr -> begin
-    let sz = (4*1024) in
+    let sz = Filesystem.Path.file_size (Filesystem.Path.from_string fn) in
     let buf = Cstdio.File.Buffer.create sz in
     Cstdio.File.fread buf sz fptr |> function
      | Ok cnt ->
-       (* Printf.printf "  read:%s\n" (Cstdio.File.Buffer.to_string buf) ; *)
        Ok (cnt, buf)
      | Error (errno,errstr) -> 
        Printf.printf "no:%d err:%s\n" errno errstr ; Error (-98)
@@ -22,9 +22,9 @@ let read_file fn =
 let write_file sz buf fn =
   Cstdio.File.fopen fn "w" |> function
   | Ok fptr -> begin
-    Cstdio.File.fwrite buf sz fptr |> function
+    let sz' = min sz (Cstdio.File.Buffer.size buf) in
+    Cstdio.File.fwrite buf sz' fptr |> function
      | Ok cnt ->
-       Printf.printf "  wrote:%d\n" cnt ;
        Cstdio.File.fclose fptr |> ignore;
        Ok cnt
      | Error (errno,errstr) -> 
@@ -34,24 +34,18 @@ let write_file sz buf fn =
 
 
 let encrypt () =
-  let aes = Aes256.Encrypt.mk k iv in
-  match read_file "/tmp/test.txt" with
+  read_file "/tmp/test.txt" |> function
   | Error code -> Printf.printf "Error: %d\n" code |> ignore
   | Ok (cnt,buf) ->
-      let enc1 = Aes256.Encrypt.process aes cnt buf in
-      let enc2 = Aes256.Encrypt.finish aes enc1 buf in
-      Printf.printf "Encrypt: %d + %d\n" enc1 enc2 |> ignore;
-      write_file (enc1 + enc2) buf "/tmp/test.crypt" |> ignore
+      let buf' = Aes256.encrypt iv k buf in
+      write_file cnt buf' "/tmp/test.crypt" |> ignore
 
 let decrypt () =
-  let aes = Aes256.Decrypt.mk k iv in
-  match read_file "/tmp/test.crypt" with
+  read_file "/tmp/test.crypt" |> function
   | Error code -> Printf.printf "Error: %d\n" code |> ignore
   | Ok (cnt,buf) ->
-      let enc1 = Aes256.Decrypt.process aes cnt buf in
-      let enc2 = Aes256.Decrypt.finish aes enc1 buf in
-      Printf.printf "Decrypt: %d + %d\n" enc1 enc2 |> ignore;
-      write_file (enc1 + enc2) buf "/tmp/test.plain" |> ignore
+      let buf' = Aes256.decrypt iv k buf in
+      write_file cnt buf' "/tmp/test.plain" |> ignore
 
 let () =
   encrypt () ; decrypt ()
