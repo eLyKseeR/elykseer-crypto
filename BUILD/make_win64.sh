50a7aa804ddbed4036b4bdb265f28240797f6b75
Alexander Diemand <codieplusplus@apax.net>
2023-04-08 13:14:53 -0700

    compilation on Win64
    Signed-off-by: Alexander Diemand <codieplusplus@apax.net>

---
+++ BUILD/make_win64.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+#
+# need to install the MinGW64 version of cmake:
+#
+# pacman -S mingw-w64-x86_64-cmake
+
+if [[ $(uname -s) != MINGW64* ]]; then
+	echo "this compilation is only valid on a MinGW64 installation"
+	exit 1
+fi
+
+# cleanup
+rm -rf CMakeCache.txt CMakeFiles src/CMakeFiles test/CMakeFiles
+
+set -e
+
+export CC=gcc
+export CXX=g++
+
+# prepare source code
+./mk_cpp.sh
+
+# where the compiled dependencies have been installed
+export PKG_CONFIG_PATH=$(pwd)/../ext/win64/lib/pkgconfig:$PKG_CONFIG_PATH
+
+# compile in Debug | Release mode
+#MODE=Debug
+MODE=Release
+cmake --fresh -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=${MODE} .
+cmake --build .
+
+# show output
+ls -l src/libelykseer-crypto*.a
+
