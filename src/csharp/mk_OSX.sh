daee9080f314dc8805952a9d8be7d5d06e5b1c5f
Alexander Diemand <codieplusplus@apax.net>
2023-12-30 13:36:57 +0100

    renamed build directory
    Signed-off-by: Alexander Diemand <codieplusplus@apax.net>

--- src/csharp/mk_OSX.sh
+++ src/csharp/mk_OSX.sh
@@ -16,10 +16,10 @@ swig -csharp -dllimport ${DLLNAME} -namespace ${NAMESPACE} aes.i
 
 VERSION="1.0.1"
 
-LDFLAGS="-L ../../BUILD/src/ -lelykseer-crypto_Debug"
+LDFLAGS="-L ../../build/src/ -lelykseer-crypto_Debug"
 CFLAGS="-Wall -fPIC -O2 -I. -compatibility_version 1.0.1 -current_version ${VERSION}"
 CFLAGS_dyn="${CFLAGS} ${LDFLAGS}"
-CFLAGS_s="${CFLAGS} ../../BUILD/src/libelykseer-crypto_Debug_s.a ../../ext/cryptopp/libcryptopp.a -lz -lboost_contract -lboost_system"
+CFLAGS_s="${CFLAGS} ../../build/src/libelykseer-crypto_Debug_s.a ../../ext/cryptopp/libcryptopp.a -lz -lboost_contract -lboost_system"
 
 export MACOSX_DEPLOYMENT_TARGET="10.12.0"
 
