8a75e5ab2b8e3ab698b0141524a0baa70a50cb8d
Alexander Diemand <codieplusplus@apax.net>
2024-07-06 15:32:01 +0200

    hmac common signature
    Signed-off-by: Alexander Diemand <codieplusplus@apax.net>

--- src/ml/lib/hmac.ml
+++ src/ml/lib/hmac.ml
@@ -1,12 +1,17 @@
 
 type b = Mlcpp_cstdio.Cstdio.File.Buffer.ta
 
-module Sha256 = struct
+module type HmacBase = sig
+  val string : string -> string -> string
+  val buffer : string -> b -> string
+end
+
+module Sha256 : HmacBase = struct
 external string : string -> string -> string = "cpp_string_hmac_sha256"
 external buffer : string -> b -> string = "cpp_buffer_hmac_sha256"
 end
 
-module Md5 = struct
+module Md5 : HmacBase = struct
 external string : string -> string -> string = "cpp_string_hmac_md5"
 external buffer : string -> b -> string = "cpp_buffer_hmac_md5"
 end
--- src/ml/lib/hmac.mli
+++ src/ml/lib/hmac.mli
@@ -1,12 +1,10 @@
 
 type b = Mlcpp_cstdio.Cstdio.File.Buffer.ta
 
-module Sha256 : sig
-val string : string -> string -> string
-val buffer : string -> b -> string
+module type HmacBase = sig
+    val string : string -> string -> string
+    val buffer : string -> b -> string
 end
-
-module Md5 : sig
-val string : string -> string -> string
-val buffer : string -> b -> string
-end
+    
+module Sha256 : HmacBase
+module Md5 : HmacBase
